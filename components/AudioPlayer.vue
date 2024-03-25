df08b276d5ce3217e8744732820b859e86d1ccc8
Liam Mclaughlin <liammcl33@gmail.com>
2024-03-25 10:22:09 +0000

    fix audio bug

--- components/AudioPlayer.vue
+++ components/AudioPlayer.vue
@@ -2,12 +2,13 @@
 
 const props = defineProps(['src', 'title'])
 const store = useModalStore()
+const { showModal } = storeToRefs(store)
 
 </script>
 
 <template>
     <div class="flex flex-col rounded-lg p-5 items-center justify-center bg-indigo-900">
         <img class="w-40 h-40 mb-5" src="~assets/thumbnails/audio-thumbnail.svg">
-        <audio @loadedmetadata="store.setLoaded(true)" controls :src=src alt=""></audio>
+        <audio v-if="showModal" @loadedmetadata="store.setLoaded(true)" controls :src=src alt=""></audio>
     </div>
 </template>
